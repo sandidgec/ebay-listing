38749c78114fe77fa3bdfe1d601159565390b735
Charles Sandidge <sandidgec@gmail.com>
2015-07-14 21:07:58 -0600

    Beginning of phase 1 - basic html and css file added

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sandidgec
+ * Date: 7/14/2015
+ * Time: 8:51 PM
+ */
+
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<link rel="ebay-listing\mystyle.css" type="text/css" href="mystyle.css">
+		<meta charset="UTF-8">
+		<title> <h1> Ebay-Listing </h1> </title>
+	</head>
+	<body>
+
+	</body>
+</html>
