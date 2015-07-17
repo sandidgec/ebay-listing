d6a9d173887cfa3371eade8f669d2da98ffd405e
Charles Sandidge <sandidgec@gmail.com>
2015-07-17 17:24:03 -0600

    added updated data design  again

--- index.php
+++ index.php
@@ -9,15 +9,17 @@
 		<div>
 		<h1> <span class="headtext"> Main Use Goal: List an Item for Sale on eBay </span> </h1>
 		</div>
+		<div id="pageborder"
 		<h2> Typical Persona </h2>
 			<p> Adult searching a large buyer market for an item who seeks
 			ease of use and convenience with minimal time commitment to see an item.
 			Appreciates a streamlined ability to list, describe and sell. </p>
 		<h2> Frustrations </h2>
-			<ul>
+			<ul> <div id="medborder"
 				<li> Time consuming process of user registration. </li>
 				<li> The time and detail needed to input in order to list an item for sale.</li>
 				<li>Deciding on a proper category and price.</li>
+				</div>
 				</ul>
 		<h2> Steps to Sell an Item on Ebay</h2>
 			<p>
@@ -26,6 +28,7 @@
 				You will then title your item, give it a description, price and add pictures.
 				</p>
 		<img class="pad-40" src="images/datadesign.svg" alt="Ebay Listing - Entity Relationship Map" />
+		</div>
 	</body>
 </html>
 
